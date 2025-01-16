8da8dbc0cc2e968cd87e22057a4fd8dbcffea006
loxenary <130627415+AngelicaAliwinata@users.noreply.github.com>
2025-01-17 04:25:10 +0700

    feat: fix integrate

--- openapi-ts.config.ts
+++ openapi-ts.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from '@hey-api/openapi-ts'
 
 export default defineConfig({
   client: '@hey-api/client-axios',
-  input: `http://localhost:5001/openapi.json`,
+  input: `https://kataloka-mobile-backend.kataloka-mobile.workers.dev/openapi.json`,
   output: {
     format: 'prettier',
     path: 'app/api',
